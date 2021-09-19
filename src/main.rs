8b7c4a13b2912e398a09ef38f24fa30ea764426d
Dakota Williams <raineforest@raineforest.me>
2021-09-19 14:02:46 -0400

    Moved to sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use actix_web::{rt::System, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{rt::System, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::convert::TryInto;
 use std::sync::mpsc;
 use std::thread;
+
 mod model;
+use model::Asset;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn db(req: HttpRequest) -> impl Responder {
+async fn db() -> impl Responder {
     let asset = model::Asset {
         upc: 0,
         count: 1,
-        unit: "beans"
+        unit: String::from("beans"),
+        common_name: String::from("beans")
     };
-    ""
+    match asset.register().await {
+        Ok(id) => HttpResponse::Ok().json(id),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
+}
+
+async fn GETbEANS() -> impl Responder {
+    let asset = Asset::get().await;
+    match asset {
+        Ok(a) => HttpResponse::Ok().json(a),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
 
 #[actix_web::main]
@@ -29,6 +44,7 @@ async fn main() {
                 .route("/", web::get().to(greet))
                 .route("/hello/{name}", web::get().to(greet))
                 .route("/db", web::get().to(db))
+                .route("/gimmeBeans", web::get().to(GETbEANS))
         })
         .bind("127.0.0.1:8080")?
         .shutdown_timeout(60) // <- Set shutdown timeout to 60 seconds
--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,33 @@
-use quaint::single::Quaint;
+use sqlx::Connection;
+use sqlx::SqliteConnection;
+use serde;
 
-pub struct Asset<'a> {
-        upc: u64,
-        count: u32,
-        unit: &'a str
+const SQLITE_URL: &str = "sqlite://snackotron.db";
+
+#[derive(sqlx::FromRow, serde::Serialize)]
+pub struct Asset {
+        pub upc: i64,
+        pub count: i32,
+        pub unit: String,
+        pub common_name: String
 }
 
-impl Asset<'_> {
-        pub async fn register(&self) -> Result<(), quaint::error::Error> {
-                let conn = quaint::single::Quaint::new("file://snackotron.db").await?;
-                conn.insert(Insert::single_into("assets")
-                        .value("upc", self.upc.to_string())
-                        .value("count", self.count.to_string())
-                        .value("unit", self.unit)
-                        .build()).await?
+impl Asset {
+        pub async fn register(&self) -> Result<i64, sqlx::Error> {
+                let mut conn = SqliteConnection::connect(SQLITE_URL).await?;
+
+                let id: i64 = sqlx::query(
+                        r#"INSERT INTO 'assets' ('upc', 'count', 'unit', 'common_name') VALUES (?, ?, ?, ?)"#)
+                        .bind(self.upc).bind(self.count).bind(&self.unit).bind(&self.common_name)
+                        .execute(&mut conn).await?.last_insert_rowid();
+
+                Ok(id)
+        }
+
+        pub async fn get() -> Result<Asset, sqlx::Error> {
+                let mut conn = SqliteConnection::connect(SQLITE_URL).await?;
+
+                Ok(sqlx::query_as::<_, Asset>(r#"SELECT * FROM assets WHERE common_name = "beans""#)
+                        .fetch_one(&mut conn).await?)
         }
 }
